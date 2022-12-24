0ae10c285b24875cb80e795580a811f95330919b
RaPGoD31 <ardakaanaydilek@gmail.com>
2022-12-24 17:52:43 +0300

    login with google

--- app.js
+++ app.js
@@ -1,72 +1,109 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-const encrypt = require('mongoose-encryption');
 const ejs = require('ejs')
+const mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate');
+mongoose.set('strictQuery', false);
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const port = 3000;
+
 const app = express();
+
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:27017/${process.env.DB_NAME}`)
 
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
-        required: [true, "Why not"]
     },
     password: {
         type: String,
-        required: [true, "WHY NOT!"]
     }
 });
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User = new mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.route("/")
     .get((req, res) => { res.render("home") });
 
 app.route("/login")
     .get((req, res) => { res.render("login") })
-    .post((req, res) => {
-        const username = req.body.username;
-        const password = req.body.password;
-        User.findOne({ email: username }, (err, found) => {
-            if (err) {
-                console.log(err)
-            } else {
-                if (found) {
-                    if (found.password === password) {
-                        res.render("secrets")
-                    } else {
-                        res.send("wrong password")
-                    }
-                } else {
-                    res.send("no user register with that  ")
-                }
-            }
-        })
-    })
+    .post(passport.authenticate('local', { // there is a lot of drama but explonation is https://github.com/londonappbrewery/Authentication-Secrets/issues/18
+        successRedirect: '/secrets',
+        failureRedirect: '/login'
+    }));
 
 app.route("/register")
     .get((req, res) => { res.render("register") })
     .post((req, res) => {
-        const user = new User({
-            email: req.body.username,
-            password: req.body.password
-        });
-        user.save((err) => {
-            if (!err) {
-                res.render('secrets');
+        User.register({ username: req.body.username }, req.body.password, (err, user) => {
+            if (err) {
+                console.log(err);
+                res.redirect('/')
             } else {
-                res.send('error')
+                passport.authenticate('local')(req, res, function () { // does not work with required: true validation xddddddd
+                    res.redirect("/secrets")
+                })
             }
         })
-
     })
+
+app.route('/secrets')
+    .get((req, res) => {
+        if (req.isAuthenticated()) {
+            res.render('secrets')
+        } else {
+            res.redirect('/login')
+        }
+    });
+
+app.route('/logout')
+    .get((req, res) => {
+        req.logout(function (err) {
+            if (err) { res.send(err); }
+            res.redirect('/');
+        });
+    });
+
+app.route('/auth/google')
+    .get(passport.authenticate('google', { scope: ['profile'] }));
 
 
 app.listen(port, () => {
